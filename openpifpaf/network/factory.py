0a8737050a159f3789a55811fa6c1f7d45559ee2
guarin <guarin.fluck@epfl.ch>
2020-12-04 09:47:26 +0100

    Remove factory changes

--- openpifpaf/network/factory.py
+++ openpifpaf/network/factory.py
@@ -178,19 +178,13 @@ def factory(
         elif head_metas is not None and head_consolidation == 'filter_and_extend':
             LOG.info('filtering for dataset heads and extending existing heads')
 
-            for hn in net_cpu.head_nets:
-                hn.meta.dataset = 'voc2012'
-
             existing_headnets = {(hn.meta.dataset, hn.meta.name): hn
                                  for hn in net_cpu.head_nets}
 
-            print(existing_headnets)
-            print(head_metas)
             headnets = []
             for meta_i, meta in enumerate(head_metas):
                 if (meta.dataset, meta.name) in existing_headnets:
                     hn = existing_headnets[(meta.dataset, meta.name)]
-                    print('matched', hn)
                     headnets.append(hn)
                     # Match head metas by name and overwrite with meta from checkpoint.
                     # This makes sure that the head metas have their head_index and
