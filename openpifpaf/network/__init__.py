2e4776bdaaac34932683fa92ee37ef66f5fe3465
guarin <guarin.fluck@epfl.ch>
2020-12-20 14:46:03 +0100

    Update

--- openpifpaf/network/__init__.py
+++ openpifpaf/network/__init__.py
@@ -1,7 +1,7 @@
 """Backbone networks, head networks and tools for training."""
 
 from .basenetworks import BaseNetwork
-from .factory import cli, configure, factory, factory_from_args, local_checkpoint_path, HEAD_FACTORIES, HEAD_TYPES
+from .factory import cli, configure, factory, factory_from_args, local_checkpoint_path, HEADS
 from .heads import HeadNetwork
 from .nets import Shell
 from .trainer import Trainer
